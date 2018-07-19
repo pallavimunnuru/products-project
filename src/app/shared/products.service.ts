95a9a7f32ad9ceff477c9f031465e0977acdcfd0
pallavimunnuru <39901012+pallavimunnuru@users.noreply.github.com>
2018-07-19 13:32:09 +0530

    Update products.service.ts

--- src/app/shared/products.service.ts
+++ src/app/shared/products.service.ts
@@ -4,14 +4,27 @@ import {Product} from './product.interface';
 import { Response } from '@angular/http';
 import {  map } from 'rxjs/operators';
 import { Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+
+ private source = new BehaviorSubject<any>(0);
+navItem$ = this.source.asObservable();
 
   constructor(private http: HttpClient) { }
   getProduct() : Observable<any> {
       return this.http.get('assets/product.json');
         
     }
+
+
+     changeNav(any) {
+    this.source.next(any);
+  }
 }
